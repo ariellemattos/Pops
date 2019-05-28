92d77248be1a5a25d87f05256f9df4fb1bfb950e
vitoriagoncalvess <17259233@CFP127ALUNO.LOCAL>
2019-05-28 10:34:03 -0300

    Tirando o campo de filtros de todo o CMS

--- cms/view/pessoa_juridica/dados.php
+++ cms/view/pessoa_juridica/dados.php
@@ -44,15 +44,7 @@ function consulta(id){
 <div class="title_paginas centralizarX">
    ADM. PESSOA JURIDICA
 </div>
-<div class="caixa_filtro centralizarX">
-  <div class="caixa_input centralizarX">
-    <form action="#">
-      <label for="fname">Buscar</label>
-      <input type="text" id="fname" name="firstname" placeholder="">
-      <input type="submit" value="Filtrar">
-    </form>
-  </div>
-</div>
+
 <div id="registros_enquete" class="centralizarX">
   <table id="tabela">
     <thead>
--- cms/view/pops_nas_escolas/dados.php
+++ cms/view/pops_nas_escolas/dados.php
@@ -46,15 +46,7 @@ function consulta(id){
   <div class="title_paginas centralizarX">
      ADM. POP'S NAS ESCOLAS
   </div>
-  <div class="caixa_filtro centralizarX">
-    <div class="caixa_input centralizarX">
-      <form action="#">
-        <label for="fname">Buscar</label>
-        <input type="text" id="fname" name="firstname" placeholder="">
-        <input type="submit" value="Filtrar">
-      </form>
-    </div>
-  </div>
+  
     <div id="registros_pops_escolas" class="centralizarX">
       <table id="tabela">
         <thead>
